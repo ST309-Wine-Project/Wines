4833e8fbbdd1150a956dc85486ff188a611843c0
Niclas Hallberg <niclascvh@gmail.com>
2020-01-20 12:24:19 +0000

    Updated data cleaning file with tidyverse verbs, started text EDA

--- 2. Project/2. Text Mining/Script1.R
+++ 2. Project/2. Text Mining/Script1.R
@@ -1,9 +1,91 @@
+# SETUP
+
+library(tidyverse)
+library(tidytext)
+library(stringr)
+library(topicmodels)
+library(gridExtra)
+library(igraph)
+library(ggraph)
+
 # TO-DO
 
 "
 1. n.a.
 
 "
+# global variable definitions
 
-wines0 <- read.csv("wine-data-tidied.csv")
+wines0 <- read_csv("wine-data-tidied.csv") %>%
+  rename('id' = 'X1')
 
+wines <- wines0
+
+custom_stop_words <- c(
+  "wine"
+)
+  
+new_stop_words <- bind_rows(
+  stop_words,
+  tibble(
+    word = custom_stop_words, 
+    lexicon = rep("CUSTOM", length(custom_stop_words))
+    )
+)
+
+# generic functions
+
+prep_bigrams <- function(df) {
+  df <- df %>%
+    unnest_tokens(word, description, token = "ngrams", n = 2) %>%
+    filter(!str_detect(word, "\\d")) %>%
+    separate(word, sep = " ", into = c("word1", "word2")) %>%
+    anti_join(new_stop_words, by = c("word1" = "word")) %>%
+    anti_join(new_stop_words, by = c("word2" = "word")) %>%
+    unite('word', word1, word2, sep = " ")
+}
+
+prep_words <- function(df) {
+  df <- df %>%
+    unnest_tokens(word, description) %>%
+    filter(!str_detect(word, "\\d")) %>%
+    anti_join(new_stop_words)
+}
+
+top_n_words <- function(df, num, var, col) {
+  graph <- df %>%
+    filter(variety %in% var) %>%
+    select(id, word) %>%
+    count(word) %>%
+    top_n(num) %>%
+    ggplot(aes(reorder(word, n), n)) +
+    geom_col(fill = col) +
+    coord_flip()
+  return(graph)
+}
+
+# text EDA
+
+red_reviews <- filter(wines, type == "red")
+white_reviews <- filter(wines, type == "white")
+
+red_words <- red_reviews %>% prep_words
+white_words <- white_reviews %>% prep_words
+
+library(wordcloud)
+
+red_cloud <- red_words %>%
+  select(word) %>%
+  count(word) %>%
+  with(wordcloud(word, n, max.words = 40, scale = c(1,3), colors = "darkred"))
+
+white_cloud <- white_words %>%
+  select(word) %>%
+  count(word) %>%
+  with(wordcloud(word, n, max.words = 40, scale = c(1,3), colors = "goldenrod2"))
+
+## NOTES
+"
+REMEMBER: sort out wine's own title when analysing its text
+"
+  
