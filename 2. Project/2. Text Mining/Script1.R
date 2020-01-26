22f61b2391e402084df5a8f9aa68344f25e57124
Niclas Hallberg <niclascvh@gmail.com>
2020-01-26 12:38:55 +0000

    Text EDA bits

--- 2. Project/2. Text Mining/Script1.R
+++ 2. Project/2. Text Mining/Script1.R
@@ -64,7 +64,7 @@ top_n_words <- function(df, num, var, col) {
   return(graph)
 }
 
-# text EDA
+### ! text EDA ! ###
 
 red_reviews <- filter(wines, type == "red")
 white_reviews <- filter(wines, type == "white")
@@ -83,6 +83,48 @@ white_cloud <- white_words %>%
   select(word) %>%
   count(word) %>%
   with(wordcloud(word, n, max.words = 40, scale = c(1,3), colors = "goldenrod2"))
+
+#bigrams node graphs
+
+#red
+red_node_graph <- red_reviews %>%
+  prep_bigrams %>%
+  count(word) %>%
+  select(word, n) %>%
+  separate(word, into = c("word1", "word2"), sep = " ") %>%
+  filter(n > 400) %>%
+  arrange(desc(n)) %>%
+  graph_from_data_frame()
+
+set.seed(2019)
+
+a <- grid::arrow(type = "closed", length = unit(0.15, "inches"))
+
+ggraph(red_node_graph, layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n), show.legend = F, arrow = a, end_cap = circle(0.03, "inches")) +
+  geom_node_point(colour = "darkred", size = 3) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1, size = 3) +
+  theme_void()
+
+#white
+white_node_graph <- white_reviews %>%
+  prep_bigrams %>%
+  count(word) %>%
+  select(word, n) %>%
+  separate(word, into = c("word1", "word2"), sep = " ") %>%
+  filter(n > 150) %>%
+  arrange(desc(n)) %>%
+  graph_from_data_frame()
+
+set.seed(2019)
+
+a <- grid::arrow(type = "closed", length = unit(0.15, "inches"))
+
+ggraph(white_node_graph, layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n), show.legend = F, arrow = a, end_cap = circle(0.03, "inches")) +
+  geom_node_point(colour = "goldenrod2", size = 3) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1, size = 3) +
+  theme_void()
 
 ## NOTES
 "
