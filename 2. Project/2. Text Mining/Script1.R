888a76b7e2ac4f98d6f2ef6e97eb3d64c7fc7afd
Niclas Hallberg <niclascvh@gmail.com>
2020-01-26 13:40:56 +0000

    added basic LDA wrt red/white

--- 2. Project/2. Text Mining/Script1.R
+++ 2. Project/2. Text Mining/Script1.R
@@ -33,6 +33,11 @@ new_stop_words <- bind_rows(
     )
 )
 
+explicit_descriptors <- wines %>%
+  unite("text", variety, type, province, region_1, region_2, sep = " ") %>%
+  select(text) %>%
+  unnest_tokens(word, text)
+
 # generic functions
 
 prep_bigrams <- function(df) {
@@ -126,6 +131,55 @@ ggraph(white_node_graph, layout = "fr") +
   geom_node_text(aes(label = name), vjust = 1, hjust = 1, size = 3) +
   theme_void()
 
+## EDA MODEL FITTING, red v. white
+
+# LDA - Latent Dirchlet Allocation
+
+red_white_key <- wines %>%
+  select(id, type) %>%
+  filter(str_detect(type, "red|white"))
+
+red_white_dtm <- red_reviews %>%
+  bind_rows(white_reviews) %>%
+  select(id, description) %>%
+  unnest_tokens(word, description) %>%
+  anti_join(new_stop_words) %>%
+  anti_join(explicit_descriptors) %>%
+  filter(!str_detect(word, "\\d")) %>%
+  count(id, word) %>%
+  arrange(id, desc(n)) %>%
+  rename(document = id) %>%
+  cast_dtm(document, word, n)
+
+red_white_lda <- LDA(red_white_dtm, k = 2, control = list(seed = 2020))
+
+red_white_topics <- tidy(red_white_lda, matrix = "beta")
+
+# graph top terms
+red_white_topics %>%
+  group_by(topic) %>%
+  top_n(10, beta) %>%
+  ungroup() %>%
+  arrange(topic, -beta) %>%
+  mutate(term = reorder_within(term, beta, topic)) %>%
+  ggplot(aes(term, beta, fill = factor(topic))) +
+  geom_col(show.legend = F) +
+  facet_wrap(~ topic, scales = "free") +
+  coord_flip() +
+  scale_x_reordered()
+
+#graph by term spread
+red_white_topics %>%
+  mutate(topic = paste0("topic", topic)) %>%
+  spread(topic, beta) %>%
+  filter(topic1 > .001 | topic2 > .001) %>%
+  mutate(log_ratio = log2(topic2 / topic1)) %>%
+  arrange(log_ratio) %>%
+  top_n(30, abs(log_ratio)) %>%
+  ggplot(aes(reorder(term, log_ratio), log_ratio)) +
+  geom_col() +
+  coord_flip()
+ 
 ## NOTES
 "
 REMEMBER: sort out wine's own title when analysing its text
